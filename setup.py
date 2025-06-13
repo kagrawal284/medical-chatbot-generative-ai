1a2a81631d36807b111d01c24bb29e71261bd6e4
Kriti Agrawal <kagrawal284@gmail.com>
2025-06-13 23:03:26 +0530

    Added basic structure of files inside project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical chatbot generative ai project',
+    version = '0.0.0',
+    author = 'Kriti Agrawal',
+    author_email = 'kriti@abc.com',
+    packages = find_packages(),
+    install_requires = []
+
+
+)
